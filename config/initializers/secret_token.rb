e34af2ab44fdc0d2aae083594410bb94615a0618
David Crutchfield <crutchfield@interactivetouchbooks.com>
2012-10-30 20:22:39 -0700

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TetrisClone::Application.config.secret_token = '814f3c479024032f4c422797136a72143a032e2f0c8ece28d7babe406b2e427cf4c55fa7d7ab741ef1af48da884de4681b7820d9ce82e768b9dc6e052a969cf1'
